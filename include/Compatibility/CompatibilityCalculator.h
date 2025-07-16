b608ea2cf61c835ad893771829749367f7c2bbd8
Matulpatel <7746matul@gmail.com>
2025-07-16 21:29:03 +0530

    Update CompatibilityCalculator.h

--- include/Compatibility/CompatibilityCalculator.h
+++ include/Compatibility/CompatibilityCalculator.h
@@ -11,5 +11,16 @@ private :
     int INTIMACY_score;
     static std::vector<std::string> emotional_q,intellectual_q,physical_q,lifegoals_q,lifestyle_q,social_q,spiritual_q;
 public :
-  
+    void check_intimacy();
+    void questions_for_compatibility_check();
+    Compatibility(const User & u_1 , const User &u_2) : user_1(u_1) , user_2(u_2) {} 
+
+
+    static void load_questions_emotional(const std::string & filename);
+    static void load_questions_social(const std::string & filename);
+    static void load_questions_spiritual(const std::string & filename);
+    static void load_questions_lifestyle(const std::string & filename);
+    static void load_questions_lifegoals(const std::string & filename);
+    static void load_questions_physical(const std::string & filename);
+    static void load_questions_intellectual(const std::string & filename);
 };
