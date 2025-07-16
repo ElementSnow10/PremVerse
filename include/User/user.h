60785727d5c6225e217bcd1c164d59996d2a6210
Matulpatel <7746matul@gmail.com>
2025-07-16 23:42:05 +0530

    Update user.h

--- include/User/user.h
+++ include/User/user.h
@@ -5,5 +5,13 @@
 
 class User{
 private :
+    std:: string name;
+    int age,birth_date,birth_month,birth_year;
+    char GENDER;
+    std:: string profession,Location;
+    static std:: vector<std:: string> hobbies;
+    static std:: vector<std::string > matched_hobbies;
 public :
+    static void load_hobbies(const std::string &filename);
+    void get_info();
 };
