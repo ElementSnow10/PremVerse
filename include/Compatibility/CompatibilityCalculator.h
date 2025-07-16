f8f327e714a761a8224d875909301a1863f5a4ca
Matulpatel <7746matul@gmail.com>
2025-07-16 21:21:40 +0530

    Update CompatibilityCalculator.h

--- include/Compatibility/CompatibilityCalculator.h
+++ include/Compatibility/CompatibilityCalculator.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <iostream> 
+#include <strings>
+#include<vector>
 
+class Compatibility {
+private :  
+    const User & user_1;
+    const User & user_2;
+    bool INTIMACY = false;
+    int INTIMACY_score;
+    static std::vector<std::string> emotional_q,intellectual_q,physical_q,lifegoals_q,lifestyle_q,social_q,spiritual_q;
+public :
+  
+};
